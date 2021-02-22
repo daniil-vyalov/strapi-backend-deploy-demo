2bd7401ff02f0dd3c7e107ccb887073cd9d62e3b
reactinside <vyalovdaniil@gmail.com>
2021-02-22 11:11:51 +0200

    feat .env example

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'beee9cb4c53e015a84459908c5663799'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5d151ba2-4d38-4fed-935e-55f3e79d5cb7'
+};
